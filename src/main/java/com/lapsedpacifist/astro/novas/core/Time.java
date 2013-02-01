efcde4727dbaaca49cc4a4aa47160c7b7b425cae
Don Willems <don_willems@me.com>
2013-02-01 18:13:47 +0100

    Added Skeleton for app_star function.
    Added a skeleton implementation for the app_star function.
    Also added classes and interfaces for structures needed by this function.
    
    Signed-off-by: Don Willems <don_willems@me.com>

---
+++ src/main/java/com/lapsedpacifist/astro/novas/core/Time.java
@@ -0,0 +1,7 @@
+package com.lapsedpacifist.astro.novas.core;
+
+import java.util.Date;
+
+public class Time extends Date {
+
+}
---
+++ src/main/java/com/lapsedpacifist/astro/novas/core/calculation/Novas.java
@@ -0,0 +1,14 @@
+package com.lapsedpacifist.astro.novas.core.calculation;
+
+import com.lapsedpacifist.astro.novas.core.CelestialObject;
+import com.lapsedpacifist.astro.novas.core.Position;
+import com.lapsedpacifist.astro.novas.core.Time;
+
+public abstract class Novas {
+	
+	public static Position getApparentPlaceOfAStar(Time time,CelestialObject cat_entry,short accuracy){
+		//TODO Implement method
+		return null;
+	}
+
+}
